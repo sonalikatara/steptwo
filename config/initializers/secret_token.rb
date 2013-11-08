eca930bef5007b2b67b77912d7588298fb43c605
sonali <sonalikatara@gmail.com>
2013-11-08 12:04:30 -0800

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Steptwo::Application.config.secret_key_base = 'a02ac5790d24c801769c595564bdb1db6c7d811e2f7e5d9a564a45a24a4956d310a309f1e94f38b99ce16ec94ad19809d10a148e56a4cff0e9e472074e1f0fa9'
